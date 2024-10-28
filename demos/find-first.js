ce376dc1447f0fe8767dd2f463a672566ff6eb36
wraydo <phillipdowner01@gmail.com>
2024-10-28 10:24:32 -0400

    find-first start

--- demos/find-first.js
+++ demos/find-first.js
@@ -1,3 +1,19 @@
-"use strict"
+"use strict";
 
 let numbers = [7, 9, 64, 60, 12, 13, 65, 62];
+
+function isOver60(arrayValue) {
+  if (arrayValue > 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let firstValueOver60 = numbers.find(isOver60);
+console.log(firstValueOver60);
+
+
+
+
+
